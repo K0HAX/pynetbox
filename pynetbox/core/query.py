a4c6aa0316e6031a758b2fb28d0612a08aeafa0c
Michael Englehorn <menglehorn@johnsonbrothers.com>
2023-05-01 17:23:48 +0000

    Trying another change

--- pynetbox/core/query.py
+++ pynetbox/core/query.py
@@ -156,7 +156,7 @@ class Request:
             "Content-Type": "application/json;",
         }
         req = self.http_session.get(
-            "{}schema".format(self.normalize_url(self.base)),
+            "{}api/schema/?format=json".format(self.normalize_url(self.base)),
             headers=headers,
         )
         if req.ok:
